6b491a985aef220d40500047d53fe77e594dbd2f
deadblue <root@dead.blue>
2019-03-21 23:58:39 +0800

    Coordinate the parameters of http.Client

--- client.go
+++ client.go
@@ -1,7 +1,11 @@
 package elevengo
 
 import (
+	"net"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"time"
 )
 
 type _UserInfo struct {
@@ -13,25 +17,69 @@ type _OfflineToken struct {
 	Time int64
 }
 
-type Client struct {
-	// basic properties for communication
-	userAgent string
+type Options struct {
+	UserAgent    string
+	DisableProxy bool
+	MaxIdleConns int
+	Debug        bool
+}
 
+type Client struct {
 	jar    http.CookieJar
 	client *http.Client
 
-	info    *_UserInfo
-	offline *_OfflineToken
+	userAgent string
+	info      *_UserInfo
+	offline   *_OfflineToken
 }
 
-func New() (client *Client, err error) {
-	return Custom(defaultUserAgent)
-}
-
-func Custom(ua string) (client *Client, err error) {
-	client = &Client{userAgent: ua}
-	if err = client.setup(); err != nil {
-		client = nil
+func New(opts *Options) *Client {
+	// core component
+	dialer := &net.Dialer{
+		Timeout: 30 * time.Second,
 	}
-	return
+	transport := &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		MaxIdleConns:          200,
+		MaxIdleConnsPerHost:   100,
+		DialContext:           dialer.DialContext,
+		IdleConnTimeout:       300 * time.Second,
+		TLSHandshakeTimeout:   30 * time.Second,
+		ResponseHeaderTimeout: 30 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	jar, _ := cookiejar.New(nil)
+	hc := &http.Client{
+		Transport: transport,
+		Jar:       jar,
+	}
+	// assemble the client
+	client := &Client{
+		jar:       jar,
+		client:    hc,
+		userAgent: defaultUserAgent,
+	}
+	// apply options
+	if opts != nil {
+		if opts.UserAgent != "" {
+			client.userAgent = opts.UserAgent
+		}
+		if opts.DisableProxy {
+			transport.Proxy = func(request *http.Request) (url *url.URL, e error) {
+				return nil, nil
+			}
+		}
+		if opts.MaxIdleConns > 0 {
+			transport.MaxConnsPerHost = opts.MaxIdleConns
+			transport.MaxIdleConns = 2 * opts.MaxIdleConns
+		}
+		if opts.Debug {
+			transport.TLSClientConfig.InsecureSkipVerify = true
+		}
+	}
+	return client
+}
+
+func Default() *Client {
+	return New(nil)
 }
--- core.go
+++ core.go
@@ -1,8 +1,6 @@
 package elevengo
 
 import (
-	"compress/flate"
-	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -37,7 +35,6 @@ func (c *Client) request(url string, qs *_QueryString, form *_Form) (data []byte
 		req.Header.Set("Content-Type", form.ContentType())
 	}
 	req.Header.Set("Accept", "*/*")
-	req.Header.Set("Accept-Encoding", "gzip, deflate")
 	req.Header.Set("Cache-Control", "no-cache")
 	req.Header.Set("Referer", apiBasic)
 	req.Header.Set("User-Agent", c.userAgent)
@@ -51,16 +48,8 @@ func (c *Client) request(url string, qs *_QueryString, form *_Form) (data []byte
 		return nil, fmt.Errorf("http error :%d", resp.StatusCode)
 	}
 	// read body
-	r := resp.Body
 	defer resp.Body.Close()
-	enc := resp.Header.Get("Content-Encoding")
-	if enc == "gzip" {
-		r, _ = gzip.NewReader(r)
-	} else if enc == "deflate" {
-		r = flate.NewReader(r)
-	}
-
-	data, err = ioutil.ReadAll(r)
+	data, err = ioutil.ReadAll(resp.Body)
 	return
 }
 
