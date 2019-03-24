ed36d0e832614e7deb5acf55083f2b6ef9d8a9ef
deadblue <root@dead.blue>
2019-03-24 17:59:04 +0800

    Implement download API

---
+++ download.go
@@ -0,0 +1,42 @@
+package elevengo
+
+import "net/url"
+
+type DownloadCookie struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type DownloadInfo struct {
+	Url       string
+	UserAgent string
+	Cookies   []*DownloadCookie
+}
+
+func (c *Client) GetDownloadInfo(pickcode string) (info *DownloadInfo, err error) {
+	// call API to get download url
+	qs := newQueryString().
+		WithString("pickcode", pickcode).
+		WithTimestamp("_")
+	result := &FileDownloadResult{}
+	err = c.requestJson(apiFileDownload, qs, nil, result)
+	if err == nil && !result.State {
+		err = apiError(result.ErrorNo)
+	}
+	if err != nil {
+		return
+	}
+	// get cookies for downloading
+	u, _ := url.Parse(result.FileUrl)
+	cookies := c.jar.Cookies(u)
+	// fill info
+	info = &DownloadInfo{
+		Url:       result.FileUrl,
+		UserAgent: c.ua,
+		Cookies:   make([]*DownloadCookie, len(cookies)),
+	}
+	for index, ck := range cookies {
+		info.Cookies[index] = &DownloadCookie{ck.Name, ck.Value}
+	}
+	return
+}
