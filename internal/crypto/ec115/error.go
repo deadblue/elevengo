3e54f395fc0f353f61091fa2611b8135aab37b67
Tomo Kunagisa <root@dead.blue>
2022-05-12 22:10:51 +0800

    Remove "internal/crypto/ec115" and "lz4".

--- internal/crypto/ec115/error.go
+++
@@ -1,5 +0,0 @@
-package ec115
-
-import "errors"
-
-var errMalformedData = errors.New("malformed data")
--- internal/crypto/ec115/util.go
+++
@@ -1,7 +0,0 @@
-package ec115
-
-import "math/rand"
-
-func randByte() byte {
-	return byte(rand.Intn(0xff))
-}
